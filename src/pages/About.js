9638d8bf6fe2126104aff18103ce629f2a114f38
NicolasM-83200 <manigand.nicolas@gmail.com>
2023-11-16 16:32:28 +0100

    first commit cours react 1

---
+++ src/pages/About.js
@@ -0,0 +1,54 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+        officia laudantium necessitatibus laborum corporis quam, tenetur modi
+        aspernatur quibusdam quo mollitia, reprehenderit error expedita sunt
+        odit unde! Voluptates, rerum rem architecto odio dolore hic earum
+        doloribus ab praesentium natus aperiam iste. Deserunt in culpa, omnis
+        sequi accusantium explicabo sapiente rem maiores corporis expedita
+        commodi nihil officiis impedit aspernatur dicta quas, quibusdam ipsum.
+        Dolorum labore numquam voluptates minima veritatis, aspernatur rem
+        explicabo, deleniti ullam vero nobis eum ratione quo itaque temporibus
+        nemo delectus voluptatibus voluptatum quas non. Obcaecati, totam.
+        Tenetur voluptatum eveniet ut praesentium nemo iste amet maiores? Quia,
+        omnis debitis.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta, eaque
+        quidem iste atque delectus magni? Ullam delectus labore, dolorum
+        doloremque ipsa nemo. Ea sit, tempora, molestias asperiores veniam eaque
+        iure eum ex beatae veritatis tenetur culpa similique sequi blanditiis
+        dolor incidunt necessitatibus fugit libero repellendus nisi laborum
+        cumque quasi. Doloremque dolorum sed animi praesentium, a fugiat tenetur
+        deleniti blanditiis sit nostrum consequuntur, veritatis incidunt?
+        Necessitatibus, tempora dicta. In totam, iusto nulla beatae natus maxime
+        earum excepturi accusamus animi repellat esse tempora error, cumque
+        dignissimos qui debitis possimus perspiciatis est commodi expedita
+        fugiat. Fuga quis aspernatur tempora esse velit dolor odio nostrum
+        veniam repellendus, expedita laborum dolorem optio. Error optio quidem
+        ab fugit harum ipsa sunt culpa! Eum ad exercitationem quidem adipisci,
+        nesciunt sint quisquam magnam quae amet voluptatem dolores error
+        temporibus ipsa nisi mollitia quo? Magnam, adipisci excepturi error
+        delectus odio temporibus facere reiciendis repellat eius, voluptas
+        eveniet. Molestias, suscipit. Reprehenderit incidunt molestias eligendi
+        eum ipsam quam dicta placeat quia tempora repellat, sapiente, officia
+        necessitatibus quas quo velit adipisci accusantium quaerat laudantium!
+        Dignissimos repudiandae totam hic, quis voluptates iure soluta quod, vel
+        repellendus ad eos quas eveniet corrupti ducimus ea suscipit! Nisi velit
+        hic id, dolorem tenetur vel beatae totam!
+      </p>
+    </div>
+  );
+};
+
+export default About;
